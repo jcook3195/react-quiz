f6f8b6e1e7a1aaa0ffd7f8fdc53aca608a5e14cc
Jordan Cook <jordan.cook@cccga.com>
2019-03-01 10:16:28 -0500

    first commit

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li><NavLink to="/">New Quiz</NavLink></li>
+      <li><NavLink to="/">Logout</NavLink></li>
+      <li><NavLink to="/" className="btn btn-floating pink lighten-1">CC</NavLink></li>
+    </ul>
+  )
+}
+
+export default SignedInLinks;
